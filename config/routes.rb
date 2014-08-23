54eb6f658763d0e5cd8fc620ced7cf8e656e781f
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-08-23 13:38:59 -0400

    added the tracks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    root to: "devise/sessions#new"
+    root to: "tracks#index"
+   resources :tracks
+   resources :events
   end
 end
